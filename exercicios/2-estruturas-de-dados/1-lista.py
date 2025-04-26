c49a8a94f3246e2b5563cf47add49ce03ea80dfa
Barga05 <lucasbargaleme05@gmail.com>
2025-04-26 01:54:07 +0000

    resolução exercicio 1/2

--- exercicios/2-estruturas-de-dados/1-lista.py
+++ exercicios/2-estruturas-de-dados/1-lista.py
@@ -1,13 +1,19 @@
 # Crie uma lista apenas com elementos numéricos
-
+misto = [1,3,56,98,70,101]
 # Crie uma lista contendo todos os tipos e estrutura de dados que você aprendeu até agora
+mista = ['python', 72, False, 'Paloma', [1,2,3], 34, 11]
 
 # Imprima na tela apenas os 5 primeiros elementos da lista
-
+print(mista [:5])
 # Crie um slice na lista para que imprima na tela os elementos de índice par
-
+elementos_misto_par = mista[::2]
+print(elementos_misto_par)
 # Remova da lista o último item
-
+mista.pop()
+print(mista)
 # Insira na lista um novo item
-
+mista.append('Lucas')
+print(mista)
 # Remova da lista um item específico
+mista.remove('Paloma')
+print(mista)
